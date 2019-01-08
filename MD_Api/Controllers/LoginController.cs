91fe48317956a110a6fd029422ed66e5a47336af
Charles Marques <charlesdevelop@hotmail.com>
2019-01-08 15:41:13 -0200

    criação login

---
+++ MD_Api/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MD_Api.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public bool Autenticar_Usuario(string pEmail, string pSenha)
+        {
+            return true;
+        }
+    }
+
+    public class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
